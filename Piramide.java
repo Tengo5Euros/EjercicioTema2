4483894245f9eb529de1157405e4c9f56d0b4cb2
Bruno <brunoesquiva12111@gmail.com>
2022-10-26 18:08:44 +0200

    Todos los ejercicios

--- Piramide.java
+++ Piramide.java
@@ -4,23 +4,22 @@ import java.util.Scanner;
 
 public class Piramide {
     public static void main(String[] args) {
-        int ancho, alto, espacios, asteriscos=1;
+        int ancho, alto, espacios, asteriscos = 1;
         Scanner input = new Scanner(System.in);
         System.out.println("Dime el ancho");
         ancho = input.nextInt();
         System.out.println("Dime el alto");
         alto = input.nextInt();
-        asteriscos=1;
-        espacios = (ancho/2) - 1;
+        asteriscos = 1;
+        espacios = (alto / 2) - 1;
         for (int i = 0; i < ancho; i++) {
             for (int j = 0; j < alto; j++) {
-                if (j!= ancho/2) {
+                if (j == espacios) {
                     System.out.print(" ");
 
-                }else if(j==ancho/2){
+                } else {
                     System.out.print("*");
                     espacios--;
-
                 }
             }
             System.out.println("\n");
